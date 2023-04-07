b67dd5085280ad31794c4d9d3cf2d7bbea51363a
Alex Barrett <akbarrett1993@gmail.com>
2023-04-07 13:12:13 -0400

    employee test added npm test fixed to run

---
+++ tests/Employee.test.js
@@ -0,0 +1,40 @@
+const Employee = require("../lib/Employee");
+
+// Employee Object
+test("Creates an employee", () => {
+  const employee = new Employee("Alex Barrett", 1, "abarrett@gmail.com");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+});
+
+//Get name from getName()
+test("Gets employee name", () => {
+  const employee = new Employee("Alex Barrett", 1, "abarrett@gmail.com");
+
+  expect(employee.getName()).toEqual(expect.any(String));
+});
+
+//Get id from getId()
+test("Gets employee id", () => {
+  const employee = new Employee("Alex Barrett", 1, "abarrett@gmail.com");
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+//Get email from getEmail()
+test("Gets employee email", () => {
+  const employee = new Employee("Alex Barrett", 1, "abarrett@gmail.com");
+
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+
+//Get role from getRole()
+test("Gets employee role", () => {
+  const employee = new Employee("Alex Barrett", 1, "abarrett@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
+});
